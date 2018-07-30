fdef815335b9a99ee23314a1d5126ff091576d94
Ilya Zheil <ilya.zheil@gmail.com>
2018-07-30 13:14:06 +0300

    added rx binding, added new extenstions

--- app/src/main/java/app/zheil/com/quiz/Extensions.kt
+++ app/src/main/java/app/zheil/com/quiz/Extensions.kt
@@ -1,6 +1,12 @@
 package app.zheil.com.quiz
 
+import android.app.Activity
+import android.support.v4.app.Fragment
 import android.view.View
+import android.widget.Toast
+import com.jakewharton.rxbinding2.view.RxView
+import org.greenrobot.eventbus.EventBus
+import java.util.concurrent.TimeUnit
 
 fun View.visible() {
     visibility = View.VISIBLE
@@ -8,4 +14,56 @@ fun View.visible() {
 
 fun View.hide() {
     visibility = View.GONE
+}
+
+//clicks
+fun View.click(onClick: () -> Unit) {
+    RxView.clicks(this)
+            .throttleFirst(500, TimeUnit.MILLISECONDS)
+            .subscribe {
+                onClick()
+            }
+}
+
+
+//Toast
+fun Activity.toast(message: String) {
+    Toast.makeText(this, message, Toast.LENGTH_LONG).show()
+}
+
+fun Activity.toast(messageId: Int) {
+    Toast.makeText(this, getString(messageId), Toast.LENGTH_LONG).show()
+}
+
+fun Fragment.toast(message: String) {
+    Toast.makeText(activity, message, Toast.LENGTH_LONG).show()
+}
+
+fun Fragment.toast(messageId: Int) {
+    Toast.makeText(activity, getString(messageId), Toast.LENGTH_LONG).show()
+}
+
+//EventBus
+//fun Any.sendEvent(event: EventBusEvent) {
+  //  EventBus.getDefault().post(event)
+//}
+
+fun Activity.registerEventBus() {
+    if (!EventBus.getDefault().isRegistered(this)) {
+        EventBus.getDefault().register(this)
+    }
+}
+
+fun Activity.unregisterEventBus() {
+    EventBus.getDefault().unregister(this)
+}
+
+fun Fragment.registerEventBus() {
+    if (!EventBus.getDefault().isRegistered(this)) {
+        EventBus.getDefault().register(this)
+    }
+}
+
+fun Fragment.unregisterEventBus() {
+    EventBus.getDefault().unregister(this)
 }
