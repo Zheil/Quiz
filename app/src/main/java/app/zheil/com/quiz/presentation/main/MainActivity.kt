2ee9f20102c426e9a02bd179a4f3978c910435af
Ilya Zheil <ilya.zheil@gmail.com>
2018-07-31 17:40:07 +0300

    added popwind

--- app/src/main/java/app/zheil/com/quiz/presentation/main/MainActivity.kt
+++ app/src/main/java/app/zheil/com/quiz/presentation/main/MainActivity.kt
@@ -1,13 +1,19 @@
 package app.zheil.com.quiz.presentation.main
 
+import android.graphics.drawable.BitmapDrawable
+import android.support.v4.content.ContextCompat
+import android.view.Gravity
+import android.view.View
+import android.view.ViewGroup
+import android.widget.PopupWindow
 import app.zheil.com.quiz.*
 import app.zheil.com.quiz.di.activity.DaggerActivityComponent
 import app.zheil.com.quiz.presentation.base.BaseActivity
 import com.arellomobile.mvp.presenter.InjectPresenter
 import kotlinx.android.synthetic.main.activity_menu.*
+import kotlinx.android.synthetic.main.popwind.view.*
 import javax.inject.Inject
 import com.michaelflisar.changelog.ChangelogBuilder
-
 
 
 class MainActivity: BaseActivity(), MainView {
@@ -17,6 +23,8 @@ class MainActivity: BaseActivity(), MainView {
 
     @Inject
     lateinit var mRouter: GlobalRouter
+
+    private lateinit var popupWindow: PopupWindow
 
     override fun setLayoutView(): Int = R.layout.activity_menu
 
@@ -38,7 +46,7 @@ class MainActivity: BaseActivity(), MainView {
 
     private fun initListeners() {
         btnStart.click {
-            mPresenter.showQuizActivity()
+            showPopWindow()
         }
 
         btnInfoApp.click {
@@ -50,6 +58,7 @@ class MainActivity: BaseActivity(), MainView {
         }
     }
 
+
     private fun initTypeFont() {
         setFontViews(tvInfoQuiz, tvVersion, tvTitleQuiz)
     }
@@ -60,6 +69,29 @@ class MainActivity: BaseActivity(), MainView {
 
     override fun showQuizActivity() {
         mRouter.showQuizActivity(this)
+    }
+
+    private fun showPopWindow() {
+        activitiesPopWindView.setBackgroundColor(ContextCompat.getColor(this, R.color.background_pop_wind))
+        activitiesPopWindView.visible()
+        val view = layoutInflater.inflate(R.layout.popwind, null, false)
+        popupWindow = PopupWindow(view, ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT, true)
+        popupWindow.setBackgroundDrawable(BitmapDrawable())
+        popupWindow.setOutsideTouchable(true)
+        popupWindow.setOnDismissListener(PopupWindow.OnDismissListener {  activitiesPopWindView.setVisibility(View.GONE) })
+        popupWindow.showAtLocation(activitiesPopWindView, Gravity.BOTTOM, 0, 0)
+        initDetailsPopupWindow(view)
+    }
+
+    private fun initDetailsPopupWindow(view: View?) {
+        view?.setOnClickListener {
+            activitiesPopWindView!!.hide()
+            popupWindow.dismiss()
+        }
+
+        view?.btnStartGame?.click {
+            mPresenter.showQuizActivity()
+        }
     }
 
 }
